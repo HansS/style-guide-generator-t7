cb2061725bd34e426d244cd65b08f46a6ce315a7
mundi morgado <mundi@tandemseven.com>
2015-12-18 18:20:19 -0800

    refactor

--- src/utils/generate.js
+++ src/utils/generate.js
@@ -6,8 +6,7 @@ import getSistemrc from './getSistemrc'
 export default function generate (options) {
   const view = options.view
   const name = options.name
-  const sistemrc = getSistemrc()
-  const View = React.createElement(view, JSON.stringify(sistemrc, null, 2))
+  const View = React.createElement(view, getSistemrc())
   const html = '<!doctype html>' + ReactDOMServer.renderToStaticMarkup(View)
   fs.outputFileSync(process.cwd() + '/sistem/' + name + '.html', html)
 }
--- src/utils/getSistemrc.js
+++ src/utils/getSistemrc.js
@@ -1,13 +1,14 @@
 import * as fs from 'fs-extra'
-import sistemrc from '../default.sistemrc'
+import defaultSistemrc from '../default.sistemrc'
 
-export default function getSistemrc () {
+export default function () {
   const sistemrcPath = process.cwd() + '/.sistemrc'
   if (!fs.existsSync(sistemrcPath)) {
     console.log('Could not find .sistemrc at', sistemrcPath)
     console.log('Creating new .sistemrc...')
-    fs.outputFileSync(sistemrcPath, JSON.stringify(sistemrc, null, 2))
+    fs.outputFileSync(sistemrcPath, JSON.stringify(defaultSistemrc, null, 2))
     console.log('.systmrc created:', sistemrcPath)
   }
-  return require(sistemrcPath)
+  var sistemrc = JSON.parse(fs.readFileSync(sistemrcPath, 'utf8'))
+  return sistemrc
 }
